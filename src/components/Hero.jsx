5a2d83ebe1b19e4ee7d75c8459041e571df40328
Aaron <138325645+Aaron-53@users.noreply.github.com>
2025-07-22 22:25:44 +0530

    adjusted sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,24 +17,24 @@ function Hero() {
       }}
     >
       <SideNavbar />
-      <div className="flex flex-col items-center text-center space-y-4 sm:space-y-6 md:space-y-8">
+      <div className="flex flex-col items-center text-center space-y-6 sm:space-y-6 md:space-y-8 w-full max-w-4xl">
         <div className="flex items-center justify-center">
           <img
             src={ieee}
             alt="IEEE Logos"
-            className="w-32 sm:w-40 md:w-48 lg:w-55 xl:w-60 h-auto z-10 hover:scale-110 transition-transform duration-300"
+            className="w-72 sm:w-48 md:w-56 lg:w-64 xl:w-80 scale-110 h-auto z-10 hover:scale-130 transition-transform duration-300 max-w-[85vw]"
           />
         </div>
         <img
           src={logo}
           alt="Real Empneo Logo"
-          className="w-64 sm:w-72 md:w-80 lg:w-90 xl:w-96 h-auto z-10 max-w-[90vw]"
+          className="w-80 sm:w-80 md:w-96 lg:w-[400px] xl:w-[450px] h-auto z-10 max-w-[90vw]"
         />
         <a
           href="https://bit.ly/EMPNEO9"
           target="_blank"
           rel="noopener noreferrer"
-          className="font-[MyFont] mt-2 sm:mt-4 px-4 sm:px-6 md:px-8 py-2 sm:py-3 border-2 border-[#D49C3E] text-[#D49C3E] text-base sm:text-lg md:text-xl rounded-md hover:bg-[#D49C3E] hover:text-white transition-all duration-300 w-auto min-w-[140px] sm:min-w-[160px] text-center inline-block"
+          className="font-[MyFont] mt-4 sm:mt-4 px-6 sm:px-6 md:px-8 py-3 sm:py-3 border-2 border-[#D49C3E] text-[#D49C3E] text-lg sm:text-lg md:text-xl rounded-md hover:bg-[#D49C3E] hover:text-white transition-all duration-300 w-auto min-w-[160px] sm:min-w-[160px] text-center inline-block"
         >
           Register Now
         </a>
