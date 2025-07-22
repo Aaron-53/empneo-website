5fda1a5adacf30305362738ac5f2ac1cca2f1d87
Aaron <138325645+Aaron-53@users.noreply.github.com>
2025-07-22 13:51:18 +0530

    navbar template

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import TopNavbar from "./TopNavbar";
+import SideNavbar from "./SideNavbar";
 
 function Hero() {
-  return <div className="w-[100vw] h-[100vh] flex items-center justify-center">Hero</div>;
+  return (
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+      <TopNavbar />
+      <SideNavbar />
+      Hero
+    </div>
+  );
 }
 
 export default Hero;
