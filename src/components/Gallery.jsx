cbdd8dcd3f36171096d367e1f6f3b4543f1bbce5
Aaron <138325645+Aaron-53@users.noreply.github.com>
2025-07-22 21:41:54 +0530

    fixed build

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Herobg from "../assets/empneobgbtr.jpg";
-import img1 from "../assets/gallery/1.png";
-import img2 from "../assets/gallery/2.png";
-import img3 from "../assets/gallery/3.png";
-import img4 from "../assets/gallery/4.png";
-import img5 from "../assets/gallery/5.png";
-import img6 from "../assets/gallery/6.png";
+import img1 from "../assets/Gallery/1.png";
+import img2 from "../assets/Gallery/2.png";
+import img3 from "../assets/Gallery/3.png";
+import img4 from "../assets/Gallery/4.png";
+import img5 from "../assets/Gallery/5.png";
+import img6 from "../assets/Gallery/6.png";
 
 const topRow = [img1, img2, img3];
 const bottomRow = [img4, img5, img6];
