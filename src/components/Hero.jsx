6d506f7aa715fb1fd58595c894a28807ec65b2a0
Namish-pa <namish4005@gmail.com>
2025-07-22 16:41:50 +0530

    about section structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import TopNavbar from "./TopNavbar";
 import SideNavbar from "./SideNavbar";
 
 function Hero() {
+
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center">
       <TopNavbar />
